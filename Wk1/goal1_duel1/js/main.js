1a442f15edcaeeb7b4ae9c5d2a04af56f7e166f1
Ebony Harris <msebonylharris@fullsail.edu>
2013-12-02 17:58:29 -0500

    created a console log to see if function works.

--- Wk1/goal1_duel1/js/main.js
+++ Wk1/goal1_duel1/js/main.js
@@ -3,3 +3,9 @@ Name: Ebony L Harris
 Date: 12/02/2013
 Assignment: Goal 1: Assignment: Duel1
 */
+
+(function(){
+
+console.log("FIGHT!!!");// This is my test to see if the self executing function is running correctly. 
+})();
+
