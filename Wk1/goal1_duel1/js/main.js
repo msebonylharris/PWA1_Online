2fca528b9edd322e6afe1b725b9f1ada05e5b669
Ebony Harris <msebonylharris@fullsail.edu>
2013-12-03 10:51:15 -0500

    Re-entered my variables. Trying to see where I went wrong. The code is properly coded, and commented.

--- Wk1/goal1_duel1/js/main.js
+++ Wk1/goal1_duel1/js/main.js
@@ -5,6 +5,46 @@ Assignment: Goal 1: Assignment: Duel1
 */
 
 (function(){
+	
+	console.log("FIGHT!!!");
+	
+//>>>>>>>>>>>>>>>>>>>>>>>>>>>PLAYER NAMES		
+	var player1Name = "Spiderman";
+	/*In the code above I have created a variable and named player1Name, I have given 
+	my variable a value of "Spiderman", which is a string. I have closed the line with a 
+	semi-colon*/
+	
+	var player2Name = "Batman";
+	/*In the code above I have created a variable and named player2Name, I have given 
+	my variable a value of "Batman", which is a string. I have closed the line with a 
+	semi-colon*/
+
+//>>>>>>>>>>>>>>>>>>>>>>>>>>>PLAYER DAMAGE
+	var player1Damage = 25;
+	/*In the code above I have created a private variable and named it player1Damage, I have given
+	this variable a number value of 25. I have closed the line with a semi-colon*/
+	
+	var player2Damage = 25;
+	/*In the code above I have created a variable and named it player1Damage, I have given
+	this variable a number value of 25. I have closed the line with a semi-colon*/
+
+//>>>>>>>>>>>>>>>>>>>>>>>>>>>PLAYER HEAlTH	
+	var player1Health = 100;
+	/* I have created a variable and called it player1Health, I gave it a number value of
+	100. The purpose of the player1Health is so that we can have a starting point for our damage to 
+	deduct from I have closed the line with a semi-colon*/
+	
+	var player2Health = 100;
+	/* I have created a variable and called it player2Health, I gave it a number value of
+	100. The purpose of the player2Health is so that we can have a starting point for our damage to 
+	deduct from I have closed the line with a semi-colon*/
+
+//>>>>>>>>>>>>>>>>>>>>>>>>>>>ROUNDS PLAYED
+	var round=0; 
+	/*I have created a variable and called it round. I have set a number value of 1. The
+	purpose of the variable "round" is give a starting point, so that for each time the fight button
+	is pushed, and we go through another loop, we can add on to this and correctly tell which level 
+	we are on. I have closed the line with a semi-colon*/
 
 })();
 
