782d36148cbebe76a12a719040e15d369203af2c
Ebony Harris <msebonylharris@fullsail.edu>
2013-12-02 21:06:53 -0500

    I had some serious trouble at this point. I was not sure why I kept comming up with the error message "Uncaught SyntaxError: Unexpected identifier", I found that this error was due to my previous training. Instead of typing "private variable", I was to just use var to identify a variable. After finding my error, I was able to catch the other error that I had trouble finding. Which was the math.round and the math.random. I am learning that this is not the same as when I use flash builder. Not sure if a lot of people have these errors, but I sure did.

--- Wk1/goal1_duel1/js/main.js
+++ Wk1/goal1_duel1/js/main.js
@@ -6,46 +6,42 @@ Assignment: Goal 1: Assignment: Duel1
 
 (function(){
 
-	console.log( "FIGHT!!!" );
+	console.log('FIGHT!!!');
 
 //>>>>>>>>>>>>>>>>>>>>>>>>>>>PLAYER NAMES		
-	private var player1Name = "Spiderman";
-	/*In the code above I have created a private variable and named player1Name, I have given 
+	var player1Name = "Spiderman";
+	/*In the code above I have created a variable and named player1Name, I have given 
 	my variable a value of "Spiderman", which is a string. I have closed the line with a 
 	semi-colon*/
 	
-	private var player2Name = "Batman";
-	/*In the code above I have created a private variable and named player2Name, I have given 
+	var player2Name = "Batman";
+	/*In the code above I have created a variable and named player2Name, I have given 
 	my variable a value of "Batman", which is a string. I have closed the line with a 
 	semi-colon*/
 
 //>>>>>>>>>>>>>>>>>>>>>>>>>>>PLAYER DAMAGE
-	private var player1Damage = math.round(math.random *10 + 16);
+	var player1Damage = 20;
 	/*In the code above I have created a private variable and named it player1Damage, I have given
-	this variable a value of math.round(math.random *10 + 16), this should give me a random number
-	from 1-25. It will also auto-populate so that the winner is always random. I have closed the line with a 
-	semi-colon*/
+	this variable a number value of 20. I have closed the line with a semi-colon*/
 	
-	private var player2Damage = math.round(math.random *10 + 11);
-	/*In the code above I have created a private variable and named it player1Damage, I have given
-	this variable a value of math.round(math.random *10 + 16), this should give me a random number
-	from 1-25. It will also auto-populate so that the winner is always random. I have closed the line with a 
-	semi-colon*/
+	var player2Damage = 25;
+	/*In the code above I have created a variable and named it player1Damage, I have given
+	this variable a number value of 25. I have closed the line with a semi-colon*/
 
 //>>>>>>>>>>>>>>>>>>>>>>>>>>>PLAYER HEAlTH	
-	private var player1Health = 100;
-	/* I have created a private variable and called it player1Health, I gave it a number value of
+	var player1Health = 100;
+	/* I have created a variable and called it player1Health, I gave it a number value of
 	100. The purpose of the player1Health is so that we can have a starting point for our damage to 
 	deduct from I have closed the line with a semi-colon*/
 	
-	private var player2Health = 100;
-	/* I have created a private variable and called it player2Health, I gave it a number value of
+	var player2Health = 100;
+	/* I have created a variable and called it player2Health, I gave it a number value of
 	100. The purpose of the player2Health is so that we can have a starting point for our damage to 
 	deduct from I have closed the line with a semi-colon*/
 
 //>>>>>>>>>>>>>>>>>>>>>>>>>>>ROUNDS PLAYED
-	private var round=1; 
-	/*I have created a private variable and called it round. I have set a number value of 1. The
+	var round=1; 
+	/*I have created a variable and called it round. I have set a number value of 1. The
 	purpose of the variable "round" is give a starting point, so that for each time the fight button
 	is pushed, and we go through another loop, we can add on to this and correctly tell which level 
 	we are on. I have closed the line with a semi-colon*/
@@ -53,6 +49,8 @@ Assignment: Goal 1: Assignment: Duel1
 /*The following function contains the code that loops through the rounds to reduce the health of each
 player*/
 	function fight(){};
+	console.log('in the fight function');
+
 
 /*The following function contains the code that determines wether there is a winner at each
 round, depending on the outcome a string is returned to tell the outcome of each fight.*/
@@ -60,6 +58,7 @@ round, depending on the outcome a string is returned to tell the outcome of each
 
 /*The following code is a call to the fight function, this code that determines wether the fight has ended before
 the ten rounds have been reached.*/
+	console.log('program start');
 	fight();
 	
 } ) ( );
