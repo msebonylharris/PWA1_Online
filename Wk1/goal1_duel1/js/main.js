1bb05cfadc451c3740aeeb4a5350b73066eaa6e4
Ebony Harris <msebonylharris@fullsail.edu>
2013-12-03 11:50:18 -0500

    Made sure everything was working and that the damage was being deducted from each player, console.log()

--- Wk1/goal1_duel1/js/main.js
+++ Wk1/goal1_duel1/js/main.js
@@ -55,6 +55,19 @@ Assignment: Goal 1: Assignment: Duel1
 		for(var i=0; i<10; i++){
 			console.log(i);
 			
+			var minDamage1 = (player1Damage * .5);
+			var minDamage2 = (player2Damage * .5);
+			var f1 = Math.floor(Math.random()*(player1Damage-minDamage1) + minDamage1);
+			var f2 = Math.floor(Math.random()*(player2Damage-minDamage2) + minDamage2);
+		
+			//console.log(f1);
+			//console.log(f2);
+			
+			player1Health-=f1;
+			player2Health-=f2;
+			
+			console.log(player1Name+":"+player1Health);			
+			console.log(player2Name+":"+player2Health);			
 		};
 	};
 	
